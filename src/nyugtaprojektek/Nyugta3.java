a5f762d102d6b83d3fcc1dda3d315caee0241417
Norbus2000 <78794087+Norbus2000@users.noreply.github.com>
2022-03-18 13:33:32 +0100

    Majdem kesz a formazott kiiras hianyzik

--- src/nyugtaprojektek/Nyugta3.java
+++ src/nyugtaprojektek/Nyugta3.java
@@ -6,26 +6,35 @@ a kedvezmény helyett legyen szervízdíj, amit hozzá kell adni a végössdzegh
 */
 public class Nyugta3 {
 
-    public static void main0(String[] args) {
+    public static void main(String[] args) {
         //String csillagok = "********************";
         vonal("*",20);
-        
+        kiiras("\n");
+        //kiiras("\n");
         //System.out.println(csillagok);
         //System.out.println("     Nyugta 3");
-        System.out.printf("%14s\n", "Nyugta 3");
+        //System.out.printf("%14s\n", "Nyugta 3");
+        szovegFormKiir("Nyugta 3");
+        kiiras("\n");
+        
         //System.out.println(csillagok);
          vonal("*",20);
+        kiiras("\n");
         
         final String HUF = "Ft";
         int tetel1 = 350, tetel2 = 90, tetel3 = 1320;
 //        System.out.printf("Tétel 1:     %d %s\n", tetel1, huf);
 //        System.out.printf("Tétel 1:      %d %s\n", tetel2, huf);
 //        System.out.printf("Tétel 3:    %d %s\n", tetel3, huf);
-        System.out.printf("%10s: %5d %s\n", "Tétel 1", tetel1, HUF);
-        System.out.printf("%10s: %5d %s\n", "Tétel 2", tetel2, HUF);
-        System.out.printf("%10s: %5d %s\n", "Tétel 3", tetel3, HUF);
+        //System.out.printf("%10s: %5d %s\n", "Tétel 1", tetel1, HUF);
+        penznemEsTetel("Tétel","ft",1);
+        penznemEsTetel("Tétel","ft",1);
+        penznemEsTetel("Tétel","ft",1);
+        //System.out.printf("%10s: %5d %s\n", "Tétel 2", tetel2, HUF);
+        //System.out.printf("%10s: %5d %s\n", "Tétel 3", tetel3, HUF);
 
         vonal("=",20);
+        kiiras("\n");
         //String duplaVonal = "====================";
         //System.out.println(duplaVonal);
         
@@ -36,6 +45,7 @@ public class Nyugta3 {
         //String szaggatottVonal = "--------------------";
         //System.out.println(szaggatottVonal);
         vonal("-", 20);
+        kiiras("\n");
         
         int szervizDijMertek = 10;
         int szervizDij = osszesen / szervizDijMertek;
@@ -58,48 +68,69 @@ public class Nyugta3 {
         *  a %10s  egy "" -t ír ki, ezzel tolom beljebb
         */
         System.out.printf("%10s%7.2f %s\n","", euro, eur);//
-        
+      
         //System.out.println(szaggatottVonal);
-        
-        System.out.println("");
+        vonal("-", 20);
+         kiiras("\n");
+        //System.out.println("");
         //String rovidVonal = "_______";
         //System.out.print(rovidVonal);
         vonal("_",7);
-        String rovidVonalValaszto = "      ";
-        System.out.print(rovidVonalValaszto);
+        
+        //String rovidVonalValaszto = "      ";
+        //System.out.print(rovidVonalValaszto);
+        vonal(" ", 6);
         //System.out.println(rovidVonal);
         vonal("_",7);
-        System.out.print(" Dátum");
-        System.out.print(rovidVonalValaszto);
-        System.out.println("   Név");
+        kiiras("\n");
+        
+        //System.out.print(" Dátum");
+        kiiras(" Dátum");
+        //System.out.print(rovidVonalValaszto);
+        kiiras("       Név");
+        kiiras("\n");
+        //vonal("_",7);
         
         //System.out.println(csillagok);
         vonal("*",20);
-        System.out.println("        CÉG");
+        kiiras("\n");
+        kiiras("        CÉG");
+        kiiras("\n");
+       //System.out.println("        CÉG");
         //System.out.println(csillagok);
         vonal("*",20);
+        kiiras("\n");
     }
-    public static void main(String[] args) {
-        kiiras("ok");
-        penznemEsTetel();
-        vonalKiirasSeged();
-        vonal("-");
+    public static void main0(String[] args) {
+        //kiiras("ok");
+        //penznemEsTetel();
+        //vonalKiirasSeged();
     }
 
     private static void kiiras(String szoveg) {
-        System.out.println(szoveg);
+        System.out.print(szoveg);
         
     }
 
     private static void vonal(String szaggatottVonal,int vonalszam) {
         for (int i = 0; i < vonalszam; i++) {
-            szaggatottVonal="";
             kiiras(szaggatottVonal);
         }
     }
 
-    private static void penznemEsTetel(String tetel,int penznem, int tetelszam) {
-        System.out.printf("%10s: %5d %s\n", "Tétel %d", tetel, penznem, tetelszam);
+    private static void penznemEsTetel(String tetel,String penznem) {
+        //System.out.printf("%10s: %5d %s\n", "Tétel %d", tetel, penznem, tetelszam);
+        //System.out.printf("%10s: %5d %s\n", "Tétel %30d", tetel,tetelszam,penznem);
+        System.out.printf("%10s: %5d %s\n", "Tétel 2", tetel, penznem);
+        //System.out.printf("%10s: %5d %s\n", "Tétel","%d", tetel, penznem,nemtom,tetelszam);
+    }
+
+    private static void szovegFormKiir(String szoveg) {
+        System.out.printf("%14s",szoveg);
+    }
+
+    private static void szamFormKiir() {
+        
     }
 
 }
